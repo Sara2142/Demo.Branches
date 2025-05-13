e8c12914b90127bca974eab44276330feeed7cb3
Sara <so013872@gmail.com>
2025-05-13 13:23:58 +0200

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -4,35 +4,41 @@ import * as josefin from "./josefin.js";
 import * as sara from "./sara.js";
 
 
-const prompt = prompt(text)
-if (text === "tilde") {
-    tilde.hello();
-}
+function handler (){
+    const prompt = prompt(text)
+    
+    if (text === "tilde") {
+        tilde.hello();
+    }
+    
+    if (text === "maja") {
+        maja.hello();
+    }
+    
+    if (text === "josefin") {
+        josefin.hello();
+    }
+    
+    if (text === "sara") {
+        sara.Hi();
+    }
+    
+    if (text === "tilde food") {
+        tilde.food();
+    }
+    
+    if (text === "maja food") {
+        maja.food();
+    }
+    
+    if (text === "josefin food") {
+        josefin.food();
+    }
+    
+    if (text === "sara food") {
+        sara.food();
+    } 
+    
 
-if (text === "maja") {
-    maja.hello();
 }
-
-if (text === "josefin") {
-    josefin.hello();
-}
-
-if (text === "sara") {
-    sara.Hi();
-}
-
-if (text === "tilde food") {
-    tilde.food();
-}
-
-if (text === "maja food") {
-    maja.food();
-}
-
-if (text === "josefin food") {
-    josefin.food();
-}
-
-if (text === "sara food") {
-    sara.food();
-} 
+Deno.serve(handler)
