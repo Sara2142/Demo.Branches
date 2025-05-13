90c17647ff33b3dedcce3a4ab4ab10f3a60d60c0
tilde-ad <tilde.davidsen@gmail.com>
2025-05-13 11:58:56 +0200

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,3 +1,18 @@
+import * as tilde from "./tilde.js";
+import * as maja from "./maja.js";
+import * as josefin from "./josefin.js";
+import * as sara from "./sara.js";
+
 
 const prompt = prompt(text)
-console.log(prompt)
+if (text === "tilde") {
+    tilde.hello();
+} else if (text === "maja") {
+    maja.hello();
+} else if (text === "josefin") {
+    josefin.hello();
+} else if (text === "sara") {
+    sara.hi();
+} else {
+    console.log("Unknown name");
+}
